b3487810e3d0d9b5449d72aeb09841efe239a4ef
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-05-15 23:32:47 +0200

    styles: added new font (doesn't work somehow), TO FIX

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,29 @@
 import 'tailwindcss/tailwind.css'
 import './globals.css'
 
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-monster',
+  display: 'swap',
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={montserrat.variable}>
+      <style global jsx>
+        {`
+          html {
+            font-family: ${montserrat.style.fontFamily};
+          }
+        `}
+      </style>
+      <body className=" font-mono">{children}</body>
     </html>
   )
 }
