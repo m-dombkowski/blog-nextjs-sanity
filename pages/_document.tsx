b5889c7c0c2396ea9aefe5d61937d813360782aa
m-dombkowski <dombkowskimateusz@gmail.com>
2024-05-22 14:19:33 +0200

    styles/refractor: added bg-color to body, passing page from ctx object to navigation

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,22 @@
 import Navigation from 'components/Navigation'
 import { Head, Html, Main, NextScript } from 'next/document'
-
+import { cn } from 'lib/utils'
 import { montserrat } from './_app'
 
-export default function Document() {
+export default function Document(ctx: { __NEXT_DATA__: { page: string } }) {
+  const page = ctx.__NEXT_DATA__.page
+
   return (
     <Html lang="en">
       <Head />
-      <body className={montserrat.className}>
-        <Navigation />
+      <body
+        className={cn(
+          montserrat.className,
+          // ' bg-gradient-to-br from-[#fffdd9] via-[#fffdd5] to-[#FFFDD0]',
+          'bg-[#fffeec]',
+        )}
+      >
+        <Navigation page={page} />
         <Main />
         <NextScript />
       </body>
