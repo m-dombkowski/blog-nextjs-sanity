77f3efd9c03dfc8b6e02996a0e6d49a4f93c3853
m-dombkowski <dombkowskimateusz@gmail.com>
2024-05-22 15:46:44 +0200

    styles: padding changes for mobile

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -4,7 +4,7 @@ export default function PageWrapper({
   children: React.ReactElement
 }) {
   return (
-    <main className="flex justify-center max-w-7xl mx-auto px-32 flex-col items-center">
+    <main className="flex justify-center max-w-7xl mx-auto pl-32 pr-16 md:px-32 flex-col items-center">
       {children}
     </main>
   )
