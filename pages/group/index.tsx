fd3fe98ede9f9f75bbf04b936d831a9e95444702
m-dombkowski <dombkowskimateusz@gmail.com>
2024-05-24 15:09:37 +0200

    feat: added searching by group on "group" page

--- pages/group/index.tsx
+++ pages/group/index.tsx
@@ -1,11 +1,55 @@
 import PageWrapper from 'components/PageWrapper'
+import { readToken } from 'lib/sanity.api'
+import { getAllPosts, getClient } from 'lib/sanity.client'
+import Link from 'next/link'
+import { SharedPageProps } from 'pages/_app'
 
-export default function Page() {
+interface PageProps extends SharedPageProps {
+  uniqueGroups: string[]
+}
+
+export default function Page(props: PageProps) {
+  const { uniqueGroups } = props
+
+  uniqueGroups.sort()
+
   return (
     <PageWrapper>
-      <div className="flex justify-center">
-        <h1>Znajdź post po grupie </h1>
-      </div>
+      <>
+        <h1 className="text-4xl my-20">Znajdź post po grupie</h1>
+
+        {uniqueGroups.map((group, i) => (
+          <Link href={`/group/${group}`} key={group}>
+            {group}
+          </Link>
+        ))}
+      </>
     </PageWrapper>
   )
+}
+
+export const getStaticProps = async (ctx) => {
+  const { draftMode = false, params = {} } = ctx
+  const client = getClient(draftMode ? { token: readToken } : undefined)
+
+  const allPosts = await getAllPosts(client)
+
+  let groups: string[] = []
+
+  allPosts.forEach((post, i) => {
+    let grupy = post.group
+
+    grupy.forEach((el, j) => {
+      groups.push(el.toLowerCase())
+    })
+  })
+
+  const uniqueGroups = [...new Set(groups)]
+  return {
+    props: {
+      draftMode,
+      token: draftMode ? readToken : '',
+      uniqueGroups,
+    },
+  }
 }
