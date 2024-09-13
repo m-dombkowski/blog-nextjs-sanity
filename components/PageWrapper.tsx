5b13cd6d9c2bd38c1f4c6cdedeab81b7b9791da7
m-dombkowski <dombkowskimateusz@gmail.com>
2024-09-13 13:34:04 +0200

    style: minor style change, changing margin top on the page wrapper

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -4,7 +4,7 @@ export default function PageWrapper({
   children: React.ReactElement
 }) {
   return (
-    <main className="mt-36 flex max-w-7xl mx-auto sm:px-12 md:px-32 flex-col items-center px-6 min-h-[calc(100vh-300px)]">
+    <main className="mt-28 flex max-w-7xl mx-auto sm:px-12 md:px-32 flex-col items-center px-6 min-h-[calc(100vh-300px)]">
       {children}
     </main>
   )
