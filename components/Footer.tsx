9e097c1013ea52aea025ac0a9dc91a60e5c7c98e
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2025-03-16 10:51:06 +0100

    style fix for footer x axis padding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import PageWrapper from './PageWrapper'
 export default function Footer() {
   return (
     <footer className=" mt-48 border-t-2 border-black py-20 from-[#fffeec] via-[#fffffb] to-white bg-gradient-to-t">
-      <div className="flex max-w-7xl mx-auto sm:pl-24 sm:pr-12 md:px-32 flex-col items-center md:pl-[72px] md:pr-6 px-6">
+      <div className="flex max-w-7xl mx-auto sm:pl-24 sm:pr-12 md:px-20 flex-col items-center px-6">
         <div className="flex w-full md:justify-between flex-wrap flex-col md:flex-row">
           <div className=" flex flex-col gap-5">
             <h1 className="text-2xl font-bold md:mb-10 mb-2">Kontakt</h1>
