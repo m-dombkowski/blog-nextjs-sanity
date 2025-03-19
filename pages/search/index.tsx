df39bd174995b0bad4a125cebceff101625fa05a
m-dombkowski <dombkowskimateusz@gmail.com>
2025-03-19 11:57:08 +0100

    removing fetching data logic from index.tsx for search page. now it's in the context file

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -2,11 +2,8 @@ import PageTransition from 'components/PageTransition'
 import PageWrapper from 'components/PageWrapper'
 import SearchByNameForm from 'components/SearchByNameForm'
 import { readToken } from 'lib/sanity.api'
-import { getAllPosts, getAllPostsSlugs, getClient } from 'lib/sanity.client'
-import { Post } from 'lib/sanity.queries'
 import { GetStaticProps } from 'next'
 import { SharedPageProps } from 'pages/_app'
-import { useEffect, useState } from 'react'
 
 interface PageProps extends SharedPageProps {
   formatedQuery: string
@@ -17,37 +14,6 @@ interface Query {
 }
 
 export default function Page(props: PageProps) {
-  const { formatedQuery } = props
-  const [isFetching, setIsFetching] = useState(false)
-  const [filteredPosts, setFilteredPosts] = useState<Post[] | []>([])
-  const [error, setError] = useState<any | null>(null)
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setIsFetching(true)
-      setError(null)
-      try {
-        const client = getClient()
-        const data = await getAllPosts(client)
-        console.log(data)
-        setFilteredPosts(
-          data.filter((post, i) => {
-            return post.title.toLowerCase().includes(formatedQuery)
-          }),
-        )
-        // setPosts(data);
-      } catch (err) {
-        console.error('Error fetching posts:', err)
-        setError(err)
-        setFilteredPosts(null)
-      } finally {
-        setIsFetching(false)
-      }
-    }
-
-    fetchPosts()
-  }, [])
-
   return (
     <PageTransition>
       <PageWrapper>
@@ -55,7 +21,7 @@ export default function Page(props: PageProps) {
           <h1 className="text-xl my-10 md:text-4xl md:my-20">
             Znajdź post którego szukasz
           </h1>
-          <SearchByNameForm initialDefaultValue="" isFetching={isFetching} />
+          <SearchByNameForm initialDefaultValue="" />
         </>
       </PageWrapper>
     </PageTransition>
